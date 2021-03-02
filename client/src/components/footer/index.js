7a182e3d1a3968ebfe7fe78726804f9636e7a56c
EWager <ethanbwager@gmail.com>
2021-03-02 18:25:01 -0500

    refactored footer to be a reusable component. tested succesfully. updated css names to be more understandable

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import "./styles.css";
 
 function Footer() {
-    return "Footer test"
+  return (
+    <footer className="mt-5 footer-background">
+      <p class="footer-font pt-3">Phone: (555) 555 5555</p>
+      <p class="footer-font mb-0 pb-3">111 Stack Lift Ave Salem NH 03070</p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
