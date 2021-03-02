001a1ff68f481e7b963995df592fd7215409f7c2
EWager <ethanbwager@gmail.com>
2021-03-02 18:35:17 -0500

    added header and footer components to pages

--- client/src/pages/registrationPage/index.js
+++ client/src/pages/registrationPage/index.js
@@ -1,5 +1,13 @@
 import React from "react";
+import Header from "../../components/header";
+import Footer from "../../components/footer";
 
 function RegistrationPage() {}
+return (
+  <>
+    <Header />
 
+    <Footer />
+  </>
+);
 export default RegistrationPage;
