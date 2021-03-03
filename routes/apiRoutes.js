0af2cd52050aaf7eaabb8cc913a1770ac40092af
Galahad <derwin499@gmail.com>
2021-03-03 10:42:50 -0500

    API employee/classes now functional ans well as employeeClassBundle

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,11 @@
 const db = require("../models");
 const getClassBundle = require("../utilities/classBundle");
 const addToClass = require("../utilities/addToClass");
+const getEmployeeClassBundle = require("../utilities/employeeClassBundle");
 
 module.exports = (app) => {
   // GET object to populate divs with class info
-  app.get("/api/classes/:id", function (req, res) {
+  app.get("/api/member/:id/classes/", function (req, res) {
     db.Class.find({})
       .sort({ start_time: 1 })
       .then((classes) => {
@@ -21,13 +22,33 @@ module.exports = (app) => {
       .catch((err) => res.json(err));
   });
 
-  //API to add member into chosen class
-  app.post("/api/addToClass", (req, res) => {
+  //API to add member into chosen class -- Requires body with class id as id and member id as memberid
+  app.post("/api/member/addToClass", (req, res) => {
     db.Class.findOne({ _id: req.body.id }).then((selectedClass) => {
       const classUpdate = addToClass(selectedClass, req.body.memberid);
       db.Class.updateOne({ _id: req.body.id }, { $set: classUpdate })
         .then(() => res.send("Success!"))
         .catch((err) => res.json(err));
     });
+  });
+
+  // GET object to populate divs with class info
+  app.get("/api/employee/:id/classes", function (req, res) {
+    db.Class.find({})
+      .sort({ start_time: 1 })
+      .then((classes) => {
+        db.Employee.findOne({ _id: req.params.id })
+          .then((currentUser) => {
+            db.Employee.find({})
+              .then((trainers) => {
+                res.json(
+                  getEmployeeClassBundle(classes, currentUser, trainers)
+                );
+              })
+              .catch((err) => res.json(err));
+          })
+          .catch((err) => res.json(err));
+      })
+      .catch((err) => res.json(err));
   });
 };
