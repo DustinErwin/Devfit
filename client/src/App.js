ca6d69f7bd6a6db0a7d1e84f8bce69daf4e98128
Galahad <derwin499@gmail.com>
2021-03-04 14:19:48 -0500

    created authentication button and connected auth0 for login security

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,20 @@ import EmployeePage from "./pages/employeePage";
 import ManagerPage from "./pages/managerPage";
 import RegistrationPage from "./pages/registrationPage";
 import MemberStore from "./pages/memberStore";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/" component={LoginPage} exact />
-          <Route path="/member" component={MemberPage} />
-          <Route path="/employee" component={EmployeePage} />
-          <Route path="/manager" component={ManagerPage} />
-          <Route path="/registration" component={RegistrationPage} />
-          <Route path="/member-store" component={MemberStore} />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route path="/" component={LoginPage} exact />
+        <Route path="/member" component={MemberPage} />
+        <Route path="/employee" component={EmployeePage} />
+        <Route path="/manager" component={ManagerPage} />
+        <Route path="/registration" component={RegistrationPage} />
+        <Route path="/member-store" component={MemberStore} />
+      </Switch>
     </div>
   );
 }
