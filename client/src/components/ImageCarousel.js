a70defc16c244e6adf5ab2952a962c7318bc4f28
Sarah <70615195+smanter82@users.noreply.github.com>
2021-03-04 19:16:08 -0500

    working on carousel, committing to pull

---
+++ client/src/components/ImageCarousel.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+
+export default function ImageCarousel(props) {
+  return (
+    <>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className={props.className}
+            src={process.env.PUBLIC_URL + props.imgUrl}
+            alt={props.altText}
+          />
+          <Carousel.Caption>
+            <h3>{props.slideLabel}</h3>
+            <h5>{props.carouselCaption1}</h5>
+            <p>{props.carouselCaption2}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
+}
--- client/src/pages/loginPage/index.js
+++ client/src/pages/loginPage/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 import Header from "../../components/header";
 import Footer from "../../components/footer";
+import ImageCarousel from "../../components/ImageCarousel";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 function LoginPage() {
   return (
     <>
       <Header />
+      <Container>
+        <Row>
+          <ImageCarousel
+            className="girlImg"
+            imgUrl="/images/girlfeetwaterbottle.jpg"
+            altText="Girl sitting on floor next to water bottle"
+            slideLabel="Refactor your body!"
+            carouselCaption1="Stretching your mind"
+            carouselCaption2="but need to stretch your body?"
+          />
+        </Row>
+      </Container>
 
       <Footer />
     </>
