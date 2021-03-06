da597a1b511ecd00eab8035dac0588880b222a9b
Galahad <derwin499@gmail.com>
2021-03-06 11:48:08 -0500

    login page now redirects users by their status with our app

--- client/src/pages/loginPage/index.js
+++ client/src/pages/loginPage/index.js
@@ -5,41 +5,45 @@ import ContentWrapper from "../../components/contentWrapper";
 import AuthenticationButton from "../../components/authenticationButton";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Redirect } from "react-router";
+import UserContext from "../../utilities/userContext";
 
 function LoginPage() {
-  // const [state, setState] = useState("member");
+  const [userInfo, setUserInfo] = useState();
+  const [userRole, setUserRole] = useState(null);
+  // const [isAuthenticated, setisAuthenticated] = useState(false)
 
   const { isAuthenticated } = useAuth0();
-  // const { user } = useAuth0();
-  // const { email } = user;
+  const { user } = useAuth0();
 
-  const loginRedirect = () => {
+  useEffect(() => {
     if (isAuthenticated) {
-      // fetch(`api/member/${email}`)
-      //  .then(response => response.json())
-      //  .then(data => setState({...state, data}));
-      switch ("manager") {
-        case "member":
-          return <Redirect to="/member" />;
-        case "employee":
-          return <Redirect to="/employee" />;
-        case "manager":
-          return <Redirect to="/manager" />;
-        default:
-          return;
-      }
+      const { email } = user;
+
+      fetch(`/api/user/${email}`)
+        .then((response) => response.json())
+        .then((currentUser) => {
+          setUserInfo({ ...userInfo, currentUser });
+          if (currentUser) {
+            setUserRole(<Redirect to={`/${currentUser.role}`} />);
+          } else {
+            setUserRole(<Redirect to={`/registration`} />);
+          }
+        });
     }
-  };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated]);
 
   return (
     <>
-      <Header />
-      <AuthenticationButton />
-      {loginRedirect()}
-      <ContentWrapper>
-        This container should contain the 4 boxes of info on sign up page.
-      </ContentWrapper>
-      <Footer />
+      <UserContext.Provider value={userInfo}>
+        <Header />
+        <AuthenticationButton />
+        {userRole ? userRole : null}
+        <ContentWrapper>
+          This container should contain the 4 boxes of info on sign up page.
+        </ContentWrapper>
+        <Footer />
+      </UserContext.Provider>
     </>
   );
 }
--- client/src/pages/registrationPage/index.js
+++ client/src/pages/registrationPage/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Header from "../../components/header";
 import Footer from "../../components/footer";
+import AuthenticationButton from "../../components/authenticationButton";
 
 function RegistrationPage() {
-    return (
-        <>
-          <Header />
-      
-          <Footer />
-        </>
-      );
+  return (
+    <>
+      <Header />
+      <AuthenticationButton />
+      <Footer />
+    </>
+  );
 }
 
 export default RegistrationPage;
