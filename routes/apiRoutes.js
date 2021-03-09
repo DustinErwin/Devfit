2d18e23107e95ab5e48be4961e717a8f1b5af93e
Jesal Mehta <jesaldmehta@gmail.com>
2021-03-09 00:29:38 -0500

    Added the Store APIs for GET Products and POST Order

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -239,4 +239,47 @@ module.exports = (app) => {
       .catch((err) => res.status(500).json(err));
   });
 
+
+  // STORE APIs
+  // GET API that gets the list of all products
+  app.get("/api/store/productList", (req, res) => {
+    db.Product.find({})
+      .then(products => res.json(products))
+      .catch((err) => res.status(500).json(err));
+  });
+
+  // POST API that saves the order to the database
+  app.post("/api/store/order", (req, res) => {
+    // Following object expected from front-end
+    // {
+    //   member_id:"",
+    //   order_details:[{
+    //     product_id:"",
+    //     price:,
+    //     quantity:
+    //   },{
+    //     product_id:"",
+    //     price:,
+    //     quantity:
+    //   }],
+    //   purchase_method:""
+    // }
+
+    let totalCost = 0;
+    const orderDetails = req.body.order_details;
+    orderDetails.forEach(orderItem =>{
+     const currentTotal = orderItem.price*orderItem.quantity;
+     totalCost += currentTotal
+    });
+    const order = new db.Order({
+      member_id: db.ObjectId(req.body.member_id),
+      purchased_items: orderDetails,
+      order_date: Date.now(),
+      total_cost: totalCost,
+      purchase_method: req.body.purchase_method
+    })
+    order.save()
+    .then((savedOrder) => res.json(savedOrder))
+    .catch(err => res.status(500).json(err))
+});
 };
