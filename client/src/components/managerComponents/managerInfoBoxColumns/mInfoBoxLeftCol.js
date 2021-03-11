d2177e19ac080ce61acf27679c084c0fe2b0a3ae
EWager <ethanbwager@gmail.com>
2021-03-10 23:38:21 -0500

    more small style changes to left column

--- client/src/components/managerComponents/managerInfoBoxColumns/mInfoBoxLeftCol.js
+++ client/src/components/managerComponents/managerInfoBoxColumns/mInfoBoxLeftCol.js
@@ -3,22 +3,27 @@ import DevBtn from "../../commonComponents/devButton/devButton";
 import "./styles.css";
 import tConvert from "../../../utilities/convertTime";
 import AuthenticationButton from "../../authenticationButton/logoutButton/logoutButton";
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
-
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function InfoBoxLeftColumn(props) {
-  console.log(props.allTrainers)
+  console.log(props.allTrainers);
   return (
     <>
-    <h2 className= "mb-3 mt-3">Trainers</h2>
-    {props.allTrainers.map((item) => {
-     return <div className="trainer-wrapper">
-     <p className=""> <DevBtn styleClass="btn-dark mr-4 ">View</DevBtn> {`${item.first_name}  ${item.last_name}`} </p>
-     </div>
-    })}
-  
-  </>)
+      <h2 className="mb-4 mt-3">Trainers</h2>
+      {props.allTrainers.map((item) => {
+        return (
+          <div className="trainer-wrapper">
+            <li className="list-item mb-3 ">
+              {" "}
+              <DevBtn styleClass="btn-dark mr-4 ">View</DevBtn>{" "}
+              {`${item.first_name}  ${item.last_name}`}{" "}
+            </li>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default InfoBoxLeftColumn;
