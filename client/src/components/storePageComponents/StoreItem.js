8326430fd2299f5a14f8d443d54babf7dd09be46
Sarah <70615195+smanter82@users.noreply.github.com>
2021-03-14 12:47:35 -0400

    deleted unused commented store code, cart container moves with scroll

--- client/src/components/storePageComponents/StoreItem.js
+++ client/src/components/storePageComponents/StoreItem.js
@@ -1,47 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Card, Button, ListGroup, ListGroupItem } from "react-bootstrap";
 import jumpRope from "../../images/store/jumpRope.jpg";
 
 function StoreItem(props) {
-    const [ selectedQuantity, updateSelectedQuantity] = useState(0);
-    const handleInputChange = (event) => {
-        const {value} = event.target;
-        const numValue = (Number(value) < 0) ? 0 : ((Number(value) > el.quantity) ? el.quantity: Number(value));
-        updateSelectedQuantity(numValue);
-    }
-    const addToCartHandler = (event) => {
-      event.preventDefault();
-      clickHandler(el._id, selectedQuantity);
-    }
-    const {el, clickHandler} = props;
-    return (
-        <Card
-        className="my-3 mx-3"
-        border="danger"
-        style={{ width: "18rem" }}
-      >
-        <Card.Img variant="top" src={jumpRope} />
-        <Card.Body>
-          <Card.Title>{el.name}</Card.Title>
-          <Card.Text>{el.description}</Card.Text>
-        </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroupItem>${el.price}</ListGroupItem>
-          <ListGroupItem>Qty: {el.quantity - selectedQuantity}</ListGroupItem>
-        </ListGroup>
-        <Card.Body>
-            <span>
-            Enter Quantity: {' '}
-            <input type="number" name="selectedQuantity" value={selectedQuantity} className="w-50" onChange={handleInputChange} min="0" max={el.quantity}/></span>
-            <br/>
-            <br/>
-          <Card.Link href="#">
-            <Button type="button" onClick={addToCartHandler}>Add To Cart</Button>
-          </Card.Link>
-        </Card.Body>
-      </Card>
-
-    );
+  const [selectedQuantity, updateSelectedQuantity] = useState(0);
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    const numValue =
+      Number(value) < 0
+        ? 0
+        : Number(value) > el.quantity
+        ? el.quantity
+        : Number(value);
+    updateSelectedQuantity(numValue);
+  };
+  const addToCartHandler = (event) => {
+    event.preventDefault();
+    clickHandler(el._id, selectedQuantity);
+  };
+  const { el, clickHandler } = props;
+  return (
+    <Card className="my-3 mx-auto" border="danger" style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={jumpRope} />
+      <Card.Body>
+        <Card.Title>{el.name}</Card.Title>
+        <Card.Text>{el.description}</Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroupItem>${el.price}</ListGroupItem>
+        <ListGroupItem>Qty: {el.quantity - selectedQuantity}</ListGroupItem>
+      </ListGroup>
+      <Card.Body>
+        <span>
+          Enter Quantity:{" "}
+          <input
+            type="number"
+            name="selectedQuantity"
+            value={selectedQuantity}
+            className="w-50"
+            onChange={handleInputChange}
+            min="0"
+            max={el.quantity}
+          />
+        </span>
+        <br />
+        <br />
+        <Card.Link>
+          <Button type="button" onClick={addToCartHandler}>
+            Add To Cart
+          </Button>
+        </Card.Link>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default StoreItem;
--- client/src/pages/memberStore/memberStore.js
+++ client/src/pages/memberStore/memberStore.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../components/commonComponents/header/header";
 import Footer from "../../components/commonComponents/footer/footer";
 import Store from "../../components/storePageComponents/StoreComponent";
@@ -7,11 +7,6 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./memberStoreStyles.css";
 import Card from "react-bootstrap/Card";
-// import ListGroup from "react-bootstrap/ListGroup";
-// import ListGroupItem from "react-bootstrap/ListGroupItem";
-// import Button from "react-bootstrap/Button";
-// import jumpRope from "../../assets/images/store/jumpRope.jpg";
-// import storeHeadingPhoto from "../../assets/images/store/storeHeadingPhoto.jpg";
 import { Cart3 } from "react-bootstrap-icons";
 import Cart from "../../components/storePageComponents/Cart";
 
@@ -28,7 +23,7 @@ function MemberStore() {
           return { product: response };
         });
         const initialCartArray = [];
-        response.forEach(product => {
+        response.forEach((product) => {
           const cartItem = {};
           cartItem.product_id = product._id;
           cartItem.product_name = product.name;
@@ -41,68 +36,38 @@ function MemberStore() {
   }, []);
 
   const handleUpdateQuantity = (productId, newQuantity) => {
-    const updatedCart = cart.map(item => (item.product_id === productId ? { ...item, quantity: newQuantity } : item));
+    const updatedCart = cart.map((item) =>
+      item.product_id === productId ? { ...item, quantity: newQuantity } : item
+    );
     setCart(updatedCart);
-  }
+  };
 
-  //shop code credit:  https://dev.to/papasanto/build-a-react-hooks-shopping-cart-with-usestate-and-useeffect-39hk
+  //shop code adapted from:  https://dev.to/papasanto/build-a-react-hooks-shopping-cart-with-usestate-and-useeffect-39hk
   return (
     <>
       <Header />
       <Container fluid className="memberStore text-center">
-        <h1 className="text-red">
-          Dev Fit Member Store
-          {/* <img
-            src={storeHeadingPhoto}
-            className="ml-2"
-            alt="sneakers, weight, jump rope"
-          ></img> */}
-        </h1>
-
+        <h1 className="text-red mb-5">Dev Fit Member Store</h1>
         <Row>
           <Col xs={8}>
-            <Store productList={productList} cartHandler={handleUpdateQuantity}></Store>
+            <Store
+              productList={productList}
+              cartHandler={handleUpdateQuantity}
+            ></Store>
           </Col>
           <Col xs={4}>
-            {/* <Row className="storeCart d-flex justify-content-center" style={{ width: "18rem" }}>
-              <Col>
-              <Cart3 className="align-self-center" size={96}></Cart3>
-                <h3>Your Cart</h3>
-              </Col>
-            </Row>
-            <Row>
-            <Cart cartItems={cart} />
-            </Row> */}
-            <Container
-              className="storeCart d-flex justify-content-center"
-              // style={{ width: "18rem" }}
-            >
+            <Container className="storeCart d-flex justify-content-center">
               <Card className="border border-danger">
                 <Cart3 className="align-self-center" size={96}></Cart3>
                 <h3>Your Cart</h3>
-                {/* <ListGroup className="list-group-flush">
-                  <ListGroupItem className="border border-danger">
-                    Cras justo odio
-                  </ListGroupItem>
-                  <ListGroupItem className="border border-danger">
-                    Dapibus ac facilisis in
-                  </ListGroupItem>
-                  <ListGroupItem className="border border-danger">
-                    Vestibulum at eros
-                  </ListGroupItem>
-                </ListGroup> */}
                 <Card.Body>
-                  {/* <Card.Link href="#">Card Link</Card.Link>
-                  <Card.Link href="#">Another Link</Card.Link> */}
                   <Cart cartItems={cart} />
-
                 </Card.Body>
               </Card>
             </Container>
           </Col>
         </Row>
       </Container>
-
       <Footer />
     </>
   );
