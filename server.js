56b0be765acc03f5e6b86d11191d5263ef2884fb
Sarah <70615195+smanter82@users.noreply.github.com>
2021-03-14 17:55:15 -0400

    committing to pull, no changes yet

--- server.js
+++ server.js
@@ -42,5 +42,3 @@ app.get("*", function (req, res) {
 app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
-
-// app.use("/uploads", express.static("uploads"));
