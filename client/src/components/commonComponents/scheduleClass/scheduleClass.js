a3de75c701c83383e3763d16f5407e332ceea3db
EWager <ethanbwager@gmail.com>
2021-03-16 17:51:40 -0400

    added info box to member page. added render logic for class to say class or classes depending on number of classes joined. Styling changes

--- client/src/components/commonComponents/scheduleClass/scheduleClass.js
+++ client/src/components/commonComponents/scheduleClass/scheduleClass.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./styles.css";
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function ScheduleClass(props) {
   return (
-    
     <Row className="m-0 pb-3 pt-3 border-to-bottom-thin ">
       <Col xs={12} className="col border-teal border-to-right pb-3 text-center">
         <h4 className=" bold text-red">{props.fitClassName} </h4>
