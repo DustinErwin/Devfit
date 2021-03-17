ba244dffb175d59990e502d6bf96a1f89349850e
Galahad <derwin499@gmail.com>
2021-03-17 10:28:57 -0400

    merging

--- client/src/App.js
+++ client/src/App.js
@@ -28,10 +28,9 @@ function App() {
   const [userRole, setUserRole] = useState(null);
   const { isAuthenticated } = useAuth0();
   const { user } = useAuth0();
-  console.log(user)
+  console.log(user);
 
   useEffect(() => {
-    console.log("isAuthenticated?", isAuthenticated);
     if (!isShopping && isAuthenticated) {
       const { email } = user;
 
