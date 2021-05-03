93617ccb23d48957ba391be07fcb167855a348a8
Ethan <ethanbwager@gmail.com>
2021-05-03 16:29:37 -0400

    added dummy logins onto login page

---
+++ client/src/components/loginCredentials/loginCredentials.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './loginCredentials.css'
+
+export const LoginCredentials = () => {
+    return (
+        <div className='login-credentials'>
+           <p className="bold">Dummy Logins</p>
+
+           <div>Member: member@code.com Pass1234</div>
+           <div>Trainer: trainer@code.com Pass1234</div>
+           <div>Manager: manager@code.com Pass1234</div>
+        </div>
+    )
+}
